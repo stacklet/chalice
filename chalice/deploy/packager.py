89d0f784aaf8a27be8d97c32f4a32b699d232f06
Tim Penhey <tim@stacklet.io>
2023-08-31 11:04:57 +1200

    chore: update integration branch (#24)
    * python 3.10
    
    * Update tests to only test new package versions for >= 3.10
    
    We're fixing the version of packages we're installing for python
    versions < 3.10 to ensure we don't regress on older packages.  Newer
    Python versions will test the updated versions and shouldn't require any
    updates to this file going forward, assuming the package versions can be
    installed >= 3.10.
    
    * Add changelog entry for Python 3.10 support
    
    * Update pip requirement from <23.1,>=9 to >=9,<23.2
    
    Updates the requirements on [pip](https://github.com/pypa/pip) to permit the latest version.
    - [Release notes](https://github.com/pypa/pip/releases)
    - [Changelog](https://github.com/pypa/pip/blob/main/NEWS.rst)
    - [Commits](https://github.com/pypa/pip/compare/9.0.0...23.1)
    
    ---
    updated-dependencies:
    - dependency-name: pip
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Add changelog entry for pip version bump
    
    * Bumping version to 1.29.0
    
    * Remove attrs from dependabot
    
    We no longer rely on attrs in our code, we've switched to dataclasses.
    
    * initial pass at 3.11
    
    * reduce number of returns
    
    * update links to powertools
    
    * bump versions for 3.11
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Larry Ogrodnek <larry@homebound.com>
    Co-authored-by: James Saryerwinnie <js@jamesls.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: github-aws-sdk-python-automation@amazon.com <aws-sdk-python-automation>

--- chalice/deploy/packager.py
+++ chalice/deploy/packager.py
@@ -81,6 +81,8 @@ class BaseLambdaDeploymentPackager(object):
         'python3.7': 'cp37m',
         'python3.8': 'cp38',
         'python3.9': 'cp39',
+        'python3.10': 'cp310',
+        'python3.11': 'cp311',
     }
 
     def __init__(
@@ -497,6 +499,8 @@ class DependencyBuilder(object):
         'cp36m': (2, 17),
         'cp37m': (2, 17),
         'cp38': (2, 26),
+        'cp310': (2, 26),
+        'cp311': (2, 26),
     }
     # Fallback version if we're on an unknown python version
     # not in _RUNTIME_GLIBC.
--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -375,8 +375,12 @@ def test_can_load_python_version():
         expected_runtime = 'python3.7'
     elif minor <= 8:
         expected_runtime = 'python3.8'
-    else:
+    elif minor <= 9:
         expected_runtime = 'python3.9'
+    elif minor <= 10:
+        expected_runtime = 'python3.10'
+    else:
+        expected_runtime = 'python3.11'
     assert c.lambda_python_version == expected_runtime
 
 
